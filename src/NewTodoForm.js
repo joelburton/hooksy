0cee86dd91d625d2a5f9dded848cf722a91bf161
Joel Burton <joel@joelburton.com>
2019-12-24 09:16:59 -0500

    Advanced: condense update down to monolithic update fn

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,28 +1,22 @@
 import React, { useState } from 'react';
 
 function NewTodoForm({add}) {
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
+  const [data, setData] = useState({title: "", body: ""});
 
-  function updateTitle(e) {
-    setTitle(e.target.value);
-  }
-
-  function updateBody(e) {
-    setBody(e.target.value)
+  function update(e) {
+    setData({...data, [e.target.name]: e.target.value});
   }
 
   function save(e) {
     e.preventDefault();
-    setTitle("");
-    setBody("");
-    add({title, body});
+    setData({title: "", body: ""});
+    add(data);
   }
 
   return (
     <form onSubmit={save}>
-      <input name="title" value={title} onChange={updateTitle}/>
-      <input name="body" value={body} onChange={updateBody}/>
+      <input name="title" value={data.title} onChange={update}/>
+      <input name="body" value={data.body} onChange={update}/>
       <button>Save</button>
     </form>
   );
